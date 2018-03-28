fec7d3977c2a39b19762be45ece34d98ccc65a4c
Dariusz Sosnowski <dsosnowski@dsosnowski.pl>
2018-03-28 21:40:09 +0200

    gnome: change night light temp to 4000

--- gnome/set-gsettings.sh
+++ gnome/set-gsettings.sh
@@ -2,7 +2,7 @@
 
 G_COLOR="org.gnome.settings-daemon.plugins.color"
 G_NIGHT_TEMP="night-light-temperature"
-G_NIGHT_TEMP_VALUE=3000
+G_NIGHT_TEMP_VALUE=4000
 
 if gsettings get "${G_COLOR}" "${G_NIGHT_TEMP}" &> /dev/null; then
 	gsettings set "${G_COLOR}" "${G_NIGHT_TEMP}" "${G_NIGHT_TEMP_VALUE}"
